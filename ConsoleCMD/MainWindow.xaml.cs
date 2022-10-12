71dbb951180d2009dc13b491835dc9b328d24f76
Ильназ Хасанов <ilnaz.hasanoff2003@gmail.com>
2022-10-12 22:03:46 +0300

    Парсинг команд

--- ConsoleCMD/MainWindow.xaml.cs
+++ ConsoleCMD/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,11 +28,28 @@ namespace ConsoleCMD
 
         private void TB_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Up)
+            if (e.Key != Key.Enter)
             {
                 e.Handled = true;
+                return;
             }
-            MessageBox.Show($"{e.Key}");
+            string[] commands = TB.Text.Split(';');
+            // command example: color red; 
+            Regex commandRegex = new Regex(@"^(?<command>\w+)\s*(?<args>.+)?");
+            foreach (string command in commands)
+            {
+                Match match = commandRegex.Match(command);
+                if (!match.Success)
+                {
+                    TB.Text += "Ошибка в команде.";
+                    break;
+                }
+                string commandName = match.Groups["command"].Value;
+                string[] args = null;
+                if (match.Groups["args"].Success)
+                    args = match.Groups["args"].Value.Split(' ');
+                
+            }
         }
     }
 }
