36ff532af4af5bab46c2ebeda5057f643c011ccd
Anudiss <liobara31@gmail.com>
2023-01-06 17:05:42 +0300

    Подключил базу данных и создал статический класс DatabaseContext для работы с базой данных

---
+++ ConsoleCMD/Resources/Connection/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace ConsoleCMD.Resources.Connection
+{
+    public static class DatabaseContext
+    {
+        public static FileSystemEntities Entities { get; }
+
+        static DatabaseContext()
+        {
+            Entities = new FileSystemEntities();
+
+            Entities.Directory.Load();
+            Entities.File.Load();
+        }
+    }
+}
